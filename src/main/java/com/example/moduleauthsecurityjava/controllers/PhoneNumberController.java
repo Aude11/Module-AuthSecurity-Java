d2e233bfbd077b4fd82929517c1f12403d48d2e4
aude <vernet.aude@gmail>
2024-08-15 15:58:21 +0100

    add back end verification

--- src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
+++ src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
@@ -7,9 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PhoneNumberController {
+
+    int PHONE_NUMBER_LENGTH = 11;
+
     @PostMapping("/phone-number")
     public String phoneNumberSubmit(@ModelAttribute PhoneNumberRequest request) {
-        System.out.println("\033[96mThe user submitted the phone number: " + request.getPhone() + "\033[0m");
-        return "Successful!";
+        if (isValidPhoneNumber(request.getPhone())) {
+            System.out.println("\033[96mThe user submitted the phone number: " + request.getPhone() + "\033[0m");
+            return "Successful!";
+        }
+        return "Not a valid phone number format";
+    }
+
+    private boolean isValidPhoneNumber (String phoneNumber) {
+        if(phoneNumber == null) return false;
+        if (phoneNumber.length() != PHONE_NUMBER_LENGTH ) return false;
+        if (phoneNumber.matches("[0-9]+")) return true;
+        return false;
     }
 }
