8f3cc4d416aceeea5ee09cbe48d15b2ba8cf9a4a
aude <vernet.aude@gmail>
2024-08-15 16:36:20 +0100

    add loggers

--- src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
+++ src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
@@ -1,5 +1,8 @@
 package com.example.moduleauthsecurityjava.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.moduleauthsecurityjava.models.PhoneNumberRequest;
@@ -16,13 +19,22 @@ public class PhoneNumberController {
             System.out.println("\033[96mThe user submitted the phone number: " + request.getPhone() + "\033[0m");
             return "Successful!";
         }
-        return "Not a valid phone number format";
+        return String.valueOf(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
     private boolean isValidPhoneNumber (String phoneNumber) {
-        if(phoneNumber == null) return false;
-        if (phoneNumber.length() != PHONE_NUMBER_LENGTH ) return false;
-        if (phoneNumber.matches("[0-9]+")) return true;
+        if(phoneNumber == null) {
+            System.out.println("\033[96mEmpyt or null phone number provided" + "\033[0m");
+            return false;
+        }
+        if (phoneNumber.length() != PHONE_NUMBER_LENGTH ) {
+            System.out.println("\033[96mInvalid lenght phone number format provided" + "\033[0m");
+            return false;
+        }
+        if (phoneNumber.matches("[0-9]+")) {
+            return true;
+        }
+        System.out.println("\033[96mInvalid phone number format provided" + "\033[0m");
         return false;
     }
 }
